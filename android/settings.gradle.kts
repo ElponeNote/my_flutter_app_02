8c3b8f7144220f1396a0b2a64a82ccf777c5a9ae
ElponeNote <uchang21@gmail.com>
2025-06-15 23:29:12 +0900

    0615_firebase configure

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,25 +1,43 @@
-// 플러터 SDK 경로를 local.properties에서 읽어오는 함수 분리
+import java.util.Properties
+import java.io.File
+
+// local.properties에서 flutter.sdk 경로 읽기 함수
 fun getFlutterSdkPath(): String {
-    val properties = java.util.Properties()
-    file("local.properties").inputStream().use { properties.load(it) }
-    val flutterSdkPath = properties.getProperty("flutter.sdk")
-    require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
-    return flutterSdkPath
+    val properties = Properties()
+    val localPropertiesFile = File(rootDir, "local.properties")
+    if (localPropertiesFile.exists()) {
+        localPropertiesFile.inputStream().use { properties.load(it) }
+        val flutterSdkPath = properties.getProperty("flutter.sdk")
+        require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
+        return flutterSdkPath
+    } else {
+        throw GradleException("local.properties file not found")
+    }
 }
 
+val flutterSdkPath = getFlutterSdkPath()
+
 pluginManagement {
-    val flutterSdkPath = getFlutterSdkPath()
-    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        gradlePluginPortal()
     }
 }
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.7.0" apply false
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services") version("4.3.15") apply false
+    // END: FlutterFire Configuration
     id("org.jetbrains.kotlin.android") version "1.8.22" apply false
 }
 
